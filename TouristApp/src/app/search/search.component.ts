494eb901f8a83590fd667cae9c593859dee8c67b
Mercy Joseph <mercysahayamj@gmail.com>
2022-10-22 03:46:03 +0530

    Added unit test

--- TouristApp/src/app/search/search.component.ts
+++ TouristApp/src/app/search/search.component.ts
@@ -10,13 +10,62 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class SearchComponent implements OnInit {
 
-  
   public branches?: CompanyDetails[]=[];
-  criteria !: string;
-  criteriaValue!: string;
-constructor(private companyService:CompanyService) { }
+  
+isSubmitted = false;
+CriteriaType: any = ['Branch Id', 'Branch Name', 'Place'];
+constructor(public fb: FormBuilder, private companyService: CompanyService) {}
+searchForm = this.fb.group({
+  criteria: ['', [Validators.required]],
+  criteriaValue : ['', [Validators.required]]
+});
+changeCriteria(e: any) {
+  this.criteria?.setValue(e.target.value, {
+    onlySelf: true,
+  });
+}
+get criteriaValue() {
+  return this.searchForm.get('criteriaValue');
+}
+// Access formcontrols getter
+get criteria() {
+  return this.searchForm.get('criteria');
+}
+onSubmit(): void {
+  console.log("welcome to submit method ");
+  this.isSubmitted = true;
+  if (!this.searchForm.valid) 
+  {
+    false;
+    console.log("Not valid");
+  } 
+  else 
+  {
+    console.log(JSON.stringify(this.searchForm.value));
+    var searchOption = this.criteria?.value;
+    var option = (searchOption as string).split(":");
+     
+    if (option.length >0)
+    {
+      searchOption = option[1].replace(" ","");
+    }
+    console.log('Selected option '+ searchOption);
 
+    this.companyService.SearchCompanies(searchOption , this.criteriaValue?.value).subscribe(data => 
+      {
+        if( data == null)
+        {
+          console.log('Not Found');
+        }
+        this.branches = data;
+        console.log('companies'+JSON.stringify(data));
 
+      }); 
+
+    console.warn('Your searc has been submitted', this.searchForm.value);
+    this.searchForm.reset();
+  }
+}
 /* SerachCompanies(criteria : string, criteriaValue :string )
 { 
  this.companyService.SearchCompanies(criteria, criteriaValue).subscribe(data => 
