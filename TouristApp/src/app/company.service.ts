f21765e6cdcb163fd79b82842f6af3e172d7c3ed
Mercy Joseph <mercysahayamj@gmail.com>
2022-10-27 01:51:08 +0530

    Azure url implement

--- TouristApp/src/app/company.service.ts
+++ TouristApp/src/app/company.service.ts
@@ -10,11 +10,13 @@ export class CompanyService {
 
   
   Url="http://localhost:9552/"; 
+  AzureURL = "https://adminapiservice20221026181952.azurewebsites.net/";
+
   constructor(private http:HttpClient) { }
 
   SearchCompanies( criteria: string, criteriaValue: string):Observable<CompanyDetails[]>
   {  
-    var url = this.Url+"api/v1/admin/"+ criteria +"/" + criteriaValue;
+    var url = this.AzureURL+"api/v1/admin/"+ criteria +"/" + criteriaValue;
    var result = this.http.get<CompanyDetails[]>(url);   
    return result;
   } 
